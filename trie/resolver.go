afc1033754d1e8f7b99e83befcc78cea9510ec5e
alex.sharov <alex.sharov@lazada.com>
2020-01-16 13:32:02 +0700

    remove unused changes

--- trie/resolver.go
+++ trie/resolver.go
@@ -3,7 +3,6 @@ package trie
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"runtime/debug"
 	"sort"
 	"strings"
@@ -50,7 +49,6 @@ type Resolver struct {
 	rss        []*ResolveSet
 	curr       bytes.Buffer // Current key for the structure generation algorithm, as well as the input tape for the hash builder
 	succ       bytes.Buffer
-	skipped    bytes.Buffer
 	value      bytes.Buffer // Current value to be used as the value tape for the hash builder
 	groups     []uint16
 	a          accounts.Account
@@ -163,7 +161,6 @@ func (tr *Resolver) finaliseRoot() error {
 	tr.curr.Reset()
 	tr.curr.Write(tr.succ.Bytes())
 	tr.succ.Reset()
-	tr.skipped.Reset()
 	if tr.curr.Len() > 0 {
 		var err error
 		var data GenStructStepData
@@ -246,30 +243,18 @@ func (tr *Resolver) Walker(keyIdx int, k []byte, v []byte) error {
 		tr.curr.Write(tr.succ.Bytes())
 		tr.succ.Reset()
 		skip := tr.currentReq.extResolvePos // how many first nibbles to skip
-		tr.skipped.Reset()
-
 		i := 0
-		var to io.ByteWriter
 		for _, b := range k {
 			if i >= skip {
-				to = &tr.succ
-			} else {
-				to = &tr.skipped
+				tr.succ.WriteByte(b / 16)
 			}
-			//nolint:errcheck
-			to.WriteByte(b / 16)
 			i++
 			if i >= skip {
-				to = &tr.succ
-			} else {
-				to = &tr.skipped
+				tr.succ.WriteByte(b % 16)
 			}
-			//nolint:errcheck
-			to.WriteByte(b % 16)
 			i++
 		}
 		tr.succ.WriteByte(16)
-		var succ = tr.succ.Bytes()
 		if tr.curr.Len() > 0 {
 			var err error
 			var data GenStructStepData
@@ -284,7 +269,7 @@ func (tr *Resolver) Walker(keyIdx int, k []byte, v []byte) error {
 					Incarnation: tr.a.Incarnation,
 				}
 			}
-			tr.groups, err = GenStructStep(tr.currentRs.HashOnly, tr.curr.Bytes(), succ, tr.hb, data, tr.groups)
+			tr.groups, err = GenStructStep(tr.currentRs.HashOnly, tr.curr.Bytes(), tr.succ.Bytes(), tr.hb, data, tr.groups)
 			if err != nil {
 				return err
 			}
@@ -315,7 +300,6 @@ func (tr *Resolver) Walker(keyIdx int, k []byte, v []byte) error {
 			tr.value.Write(v)
 			tr.fieldSet = AccountFieldSetNotAccount
 		}
-		tr.hb.invalidatePrefixes = []int{}
 	}
 	return nil
 }
